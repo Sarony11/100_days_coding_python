fd6769574244c3800ccb05a2f9457add2b511da3
Saul Fernandez <sarony11@gmail.com>
2023-04-10 13:58:06 +0200

    day2: add excercise 1 doc

--- 02 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/day2_excercise_01.py
+++ 02 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/day2_excercise_01.py
@@ -1,3 +1,10 @@
+"""
+This excercise shows:
+- How strings can be split into digits/characters
+- How to convert string characters into numerical digits
+- How to operate with int digits (impossible with string characters)
+"""
+
 number = input("Type a number: ")
 sum_numbers = 0
 for position in number:
