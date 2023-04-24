d51752cc226c47479021ef4f4eba9502cf8f0a92
Saul Fernandez <sarony11@gmail.com>
2023-04-24 12:23:35 +0200

    doc: creating user class and playing with positional atributes

--- 17 Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/class_theory.py
+++ 17 Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/class_theory.py
@@ -5,9 +5,12 @@ class User:
     def __init__(self, user_id, user_name="Paco"):
         self.id = user_id
         self.name = user_name
+        self.folowers_n = 0
+        self.followers_list = []
         
-user_1 = User(user_name="Saul",user_id=12)
-             
+user_1 = User(user_name="Saul", user_id="00001")
+
+print(user_1.id)
 print(user_1.name)
 
 user_1.name = "Samuel"
