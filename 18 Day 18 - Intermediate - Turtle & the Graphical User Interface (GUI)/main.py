0c8b1560a4e6385d86561354efd437839c3adfce
Saul Fernandez <sarony11@gmail.com>
2023-04-24 19:17:51 +0200

    doc: turtle

---
+++ 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timi_the_turtle = Turtle()
+timi_the_turtle.shape("circle")
+timi_the_turtle.color("red")
+timi_the_turtle.forward(100)
+
+
+screen = Screen()
+screen.exitonclick()
