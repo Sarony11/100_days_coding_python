19e0da3549068db60377556161c4db800459e94f
Saul Fernandez <sarony11@gmail.com>
2023-04-25 10:47:58 +0200

    feat: add draw_painting function

--- 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/hirst-painting/main.py
+++ 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/hirst-painting/main.py
@@ -7,30 +7,60 @@ import colorgram
 COLOR_VARIETY = 24
 colors = colorgram.extract('hirst-painting.webp', COLOR_VARIETY)
 
-o = Turtle()
-Screen().colormode(255)
-
-def create_RGB_color_palette(palette):
+def create_RGB_color_palette(color_palette):
     rgb_palette = []
-    for c in palette:
+    for c in color_palette:
         r = c.rgb.r
         g = c.rgb.g
         b = c.rgb.b
         rgb_palette.append((r,g,b))
     return rgb_palette
 
-def random_color():
-    """Return a random color in RGB"""
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    print(r,g,b)
-    return (r,g,b)
+def random_color(color_palette):
+    """Return a random color in RGB from a list"""
+    return random.choice(color_palette)
+
+def draw_painting():
+    for i in range(20):
+        for j in range(20):
+            o.pencolor(random_color(painting_rgb_palette))
+            o.dot(dot_size)
+            o.forward(dot_size + dot_spacing_x)
+        o.backward(dot_size * 20 + dot_spacing_x * 20)
+        o.right(90)
+        o.forward(dot_size + dot_spacing_y)
+        o.left(90)
+    return
 
 # Create the color palette based in the hirst painting
 painting_rgb_palette = create_RGB_color_palette(colors)
 
+# Setup turtle
+o = Turtle()
+Screen().colormode(255)
+o.speed(0)
+o.seth(0)
+o.width(5)
 
+# Get the screen dimensions
+screen_width = Screen().window_width()
+screen_height = Screen().window_height()
+
+# Calculate the spacing between dots
+dot_size = 10
+dot_spacing_x = (screen_width - dot_size * 20) / 21  # 20 dots with 21 spacings
+dot_spacing_y = (screen_height - dot_size * 20) / 21
+
+# Calculate the position of the top-left dot
+x = -screen_width/2 + dot_size/2 + dot_spacing_x/2
+y = screen_height/2 - dot_size/2 - dot_spacing_y/2
+
+# Move the turtle to the top-left corner of the screen
+o.penup()
+o.goto(x, y)
+
+# PAINT!
+draw_painting()
 
 
 Screen().exitonclick()
