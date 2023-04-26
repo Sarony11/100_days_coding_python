306688e4da15fc48774951a8e8e23aa427780fb4
Saul Fernandez <sarony11@gmail.com>
2023-04-26 14:15:01 +0200

    doc: create examble of class inheritance

--- 21 Day 21 - Intermediate - Build the Snake Game Part 2_ Inheritance & List Slicing/class_inheritance.py
+++ 21 Day 21 - Intermediate - Build the Snake Game Part 2_ Inheritance & List Slicing/class_inheritance.py
@@ -12,6 +12,11 @@ class Fish(Animal):
     def swim(self):
         print("Swim")
 
+    def breathe(self):
+        super().breathe()
+        print("Breathe under water")
+
 nemo = Fish()
 nemo.swim()
 print(nemo.num_eyes)
+nemo.breathe()
