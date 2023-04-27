a0fb0de3048d14d15d9410f6b519751f489e53ad
Saul Fernandez <sarony11@gmail.com>
2023-04-27 18:52:57 +0200

    feat: add paddle_1 and paddle_2 using same class

--- 22 Day 22 - Intermediate - Build Pong Arcade Game/main.py
+++ 22 Day 22 - Intermediate - Build Pong Arcade Game/main.py
@@ -2,26 +2,29 @@ from turtle import Screen
 import time
 from paddle import Paddle
 
-SCREEN_WIDTH = 600
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 game_on = True
 
 s = Screen()
 s.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 s.bgcolor("black")
-s.title("Snake Game")
+s.title("Ping Pong")
 s.tracer(0,0)
-p1 = Paddle(SCREEN_HEIGHT)
-#p2 = Paddle(SCREEN_HEIGHT)
+s.delay(10)
+p1 = Paddle(SCREEN_WIDTH,SCREEN_HEIGHT)
+p2 = Paddle(-SCREEN_WIDTH,SCREEN_HEIGHT)
 
 s.listen()
-s.onkey(fun=p1.move_up,key="w")
-s.onkey(fun=p1.move_down,key="s")
+s.onkey(lambda: p1.move_up(),key="w")
+s.onkey(lambda: p1.move_down(),key="s")
+s.onkey(lambda: p2.move_up(),key="i")
+s.onkey(lambda: p2.move_down(),key="k")
 
 
 while game_on:
     s.update()
-    time.sleep(0.1)
+    time.sleep(0.001)
 
 
 
--- 22 Day 22 - Intermediate - Build Pong Arcade Game/paddle.py
+++ 22 Day 22 - Intermediate - Build Pong Arcade Game/paddle.py
@@ -1,31 +1,24 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self,height):
+    def __init__(self,width,height):
         super().__init__()
-        self.paddle = []
-        self.create_paddle(height)
-        return
-    
-    def create_paddle(self,height):
-        pos = 20
-        for n in range(3):
-            part = Turtle("square")
-            part.color("white")
-            part.penup()
-            part.setpos((-height/2 + (height/20)),pos)
-            self.paddle.append(part)
-            pos -= 20
+        self.shape("square")
+        self.shapesize(1,5,None)
+        self.color("white")
+        self.penup()
+        self.screen_width = width
+        self.screen_height = height
+        self.setpos(-(width/2)+(width*0.1), 0)
+        self.seth(90)
         return
         
     def move_up(self):
-        for part in self.paddle:
-            part.seth(90)
-            part.forward(20)
-        return
+        if self.ycor() < (self.screen_height/2)-(self.screen_height*0.1):
+            self.forward(20)
+            return
 
     def move_down(self):
-        for part in self.paddle[::-1]:
-            part.seth(270)
-            part.forward(20)
-        return
+        if self.ycor() > (-self.screen_height/2)+(self.screen_height*0.1):
+            self.backward(20)
+            return
