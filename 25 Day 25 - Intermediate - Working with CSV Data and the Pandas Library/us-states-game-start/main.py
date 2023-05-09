528c320b9270a2ce0644333deba3abf96e6719aa
Saul Fernandez <sarony11@gmail.com>
2023-05-09 11:39:43 +0200

    feat: prepare screen with image

---
+++ 25 Day 25 - Intermediate - Working with CSV Data and the Pandas Library/us-states-game-start/main.py
@@ -0,0 +1,12 @@
+import turtle
+
+image = "blank_states_img.gif"
+screen = turtle.Screen()
+screen.title("US STATE GAME")
+screen.addshape(image)
+
+t = turtle.Turtle()
+t.shape(image)
+
+
+screen.exitonclick()
