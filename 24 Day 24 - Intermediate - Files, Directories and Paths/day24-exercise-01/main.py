28d3c2e0b550e78a5976fb4a951e2bbc97029442
Saul Fernandez <sarony11@gmail.com>
2023-05-09 04:27:42 +0200

    doc: added doc and different open modes

--- 24 Day 24 - Intermediate - Files, Directories and Paths/day24-exercise-01/main.py
+++ 24 Day 24 - Intermediate - Files, Directories and Paths/day24-exercise-01/main.py
@@ -1,3 +1,14 @@
 file = open("random.txt")
 content = file.read()
 print(content)
+file.close()
+
+"""Using with allows python to self-manage that when we have ended to work with the file, this is going to be closed.
+Regarding open(), we can use mode="r", mode="w" or mode="a" among others to control the behavior of the file manipulation."""
+with open("random.txt", mode="a") as file:
+    file.write("\nNew text.")
+    print(file)
+
+with open("random.txt", mode="r") as file:
+    content = file.read()
+    print(content)
