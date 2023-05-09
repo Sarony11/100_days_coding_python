61983ccb4aa4749b7e3592a4236ce6b839760666
Saul Fernandez <sarony11@gmail.com>
2023-05-09 08:39:08 +0200

    feat: create temp list with csv library

--- 25 Day 25 - Intermediate - Working with CSV Data and the Pandas Library/25-exercise-01.py
+++ 25 Day 25 - Intermediate - Working with CSV Data and the Pandas Library/25-exercise-01.py
@@ -2,3 +2,16 @@
     data = file.readlines()
 
 print(data) """
+
+import csv
+
+temperatures = []
+with open("226 weather-data.csv") as file:
+    data = csv.reader(file)
+    for row in data:
+        try:
+            temperatures.append(int(row[1]))
+        except:
+            None
+
+    print(temperatures)
