7540a97b2c5e6298d45f086814487828ba38ac22
Saul Fernandez <sarony11@gmail.com>
2023-05-31 21:37:32 +0200

    feat: working with .grid() for geometry layout

---
+++ 27 Day 27 - Intermediate - Tkinter, _args, __kwargs and Creating GUI Programs/day27-exercise-5.py
@@ -0,0 +1,36 @@
+# https://www.pythonguis.com/faq/pack-place-and-grid-in-tkinter/
+
+#import tkinter
+from tkinter import *
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+# Labels
+my_label = Label(text="I am a label", font=("Arial",24,"bold"))
+my_label.grid(column=0, row=0)
+
+my_label["text"] = "New Text"
+my_label.config(text="New text")
+
+#Button
+
+def button_clicked():
+    my_label.config(text="I was clicked")
+
+# This function updates the text label with the text in my_entry box.
+def label_text_on_entry():
+    my_label.config(text=my_entry.get())
+
+# I reference this button to the function button_clicked
+button = Button(text="Click Me", command=label_text_on_entry)
+button.grid(column=1, row=1)
+
+# Entry
+my_entry = Entry(width=10)
+my_entry.grid(column=2, row=2)
+
+
+window.mainloop()
+
